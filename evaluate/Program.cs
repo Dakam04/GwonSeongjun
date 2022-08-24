d1723eb97e7627d32d2843804dbed72471e207c9
Dakam04 <jjase5020@naver.com>
2022-08-25 00:38:33 +0900

    tmp

--- evaluate/Program.cs
+++ evaluate/Program.cs
@@ -10,6 +10,14 @@ namespace evaluate
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("계산 식을 입력해주세요");
+            string formula = Console.ReadLine();
+            string[] formulaSplit = formula.Split(' ');
+            for (int a = 0 ; a < formulaSplit.Length; a++)
+            {
+                Console.WriteLine(formulaSplit[a]);
+            }
+            
         }
     }
 }
