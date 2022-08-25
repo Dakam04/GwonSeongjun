8425ccfee227b75f5d1a65cdbfb695dd6cd23428
Dakam04 <jjase5020@naver.com>
2022-08-25 12:27:17 +0900

    tmp2

--- evaluate/Program.cs
+++ evaluate/Program.cs
@@ -4,20 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace evaluate
+namespace Evaluate
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("계산 식을 입력해주세요");
-            string formula = Console.ReadLine();
-            string[] formulaSplit = formula.Split(' ');
-            for (int a = 0 ; a < formulaSplit.Length; a++)
+            evaluate(Console.ReadLine());
+            Console.ReadLine(); // pause
+        }
+        static int evaluate(string formula)
+        {
+            string[] formulaArray = formula.Split(' ');
+            List<string> formulaList = formulaArray.ToList();
+
+            for (int a = 0; a < formulaList.Count; a++) // 괄호 연산
             {
-                Console.WriteLine(formulaSplit[a]);
+
             }
-            
+
+            while (true) //곱하기 나누기 연산
+            {
+                int a = 0;
+                if (0 <= formulaList.FindIndex(x => x.Contains("*"))) //만약 *가 없으면 -1이 반환되서 false
+                {
+                    a = formulaList.FindIndex(x => x.Contains("*"));
+                    formulaList[a - 1] = (int.Parse(formulaList[a - 1]) * int.Parse(formulaList[a + 1])).ToString();// n1 + n2 일때 n1에 계산된 값 넣고 +, n2 요소 삭제
+                    formulaList.RemoveAt(a);
+                    formulaList.RemoveAt(a);
+                }
+                else if (0 <= formulaList.FindIndex(x => x.Contains("/")))
+                {
+                    a = formulaList.FindIndex(x => x.Contains("/"));    
+                    formulaList[a - 1] = (int.Parse(formulaList[a - 1]) / int.Parse(formulaList[a + 1])).ToString();
+                    formulaList.RemoveAt(a);
+                    formulaList.RemoveAt(a);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int a = 0; a < formulaList.Count;) //더하기 빼기 연산
+            {
+                switch (formulaList[a])
+                {
+                    case "-":
+                        formulaList[a - 1] = (int.Parse(formulaList[a - 1]) - int.Parse(formulaList[a + 1])).ToString();
+                        formulaList.RemoveAt(a);
+                        formulaList.RemoveAt(a);
+                        break;
+
+                    case "+":
+                        formulaList[a - 1] = (int.Parse(formulaList[a - 1]) + int.Parse(formulaList[a + 1])).ToString();
+                        formulaList.RemoveAt(a);
+                        formulaList.RemoveAt(a);
+                        break;
+
+                    default:
+                        a++;
+                        break;
+                }
+            }
+
+            for(int a = 0; a < formulaList.Count; a++)
+            {
+                Console.WriteLine(formulaList[a]);
+            }
+            return 1;
         }
     }
 }
--- evaluate/Properties/Settings.Designer.cs
+++ evaluate/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace evaluate.Properties {
+namespace Evaluate.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
