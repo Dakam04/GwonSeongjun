989d48a3163ccc2930b8606f8373fa79829b99bf
Dakam04 <jjase5020@naver.com>
2022-08-26 16:19:24 +0900

    Beta Test Version 2

--- evaluate/Program.cs
+++ evaluate/Program.cs
@@ -9,13 +9,67 @@ namespace Evaluate
     class Program
     {
 
+        static List<string> CreateList(string formula)
+        {
+            formula = formula.Replace(@"\s", "");
+            List<string> formulaList = new List<string>();
+            string strPiece = "";
+            foreach (char tmp in formula)
+            {
+                switch (tmp)
+                {
+                    case ' ': //만약 공백 문자면 그냥 넘긴다.
+                        break;
+
+                    case '*':
+                        formulaList.Add(strPiece);
+                        formulaList.Add("*");
+                        strPiece = null;
+                        break;
+
+                    case '/':
+                        formulaList.Add(strPiece);
+                        formulaList.Add("/");
+                        strPiece = null;
+                        break;
+
+                    case '+':
+                        formulaList.Add(strPiece);
+                        formulaList.Add("+");
+                        strPiece = null;
+                        break;
+
+                    case '-':
+                        if ((strPiece == " ") | (strPiece == null) | (strPiece == "")) //숫자가 음수 일 경우를 대비해서 전 루틴에서 기호가 나오고 - 가 한번 더 나오면 음수로 저장
+                        {
+                            strPiece += tmp.ToString();
+                        }
+                        else
+                        {
+                            formulaList.Add(strPiece);
+                            formulaList.Add("-");
+                            strPiece = null;
+                        }
+                        break;
+
+                    default:
+                        strPiece += tmp.ToString();
+                        break;
+
+                }
+            }
+            formulaList.Add(strPiece);
+            formulaList.RemoveAll(x => String.IsNullOrWhiteSpace(x));
+            return formulaList;
+        }
+
         static double Calculation(List<string> formulaList)
         {
             /*
             while (true) //제곱 연산자
             {
                 int a = 0;
-                if (0 <= formulaList.FindIndex(x => x.Contains("^")))
+                if (0 <= formulaList.FindIndex(x => x.Contains(" ^ ")))
                 {
                     a = formulaList.FindIndex(x => x.Contains("^"));
                     formulaList[a - 1] = (int.Parse(formulaList[a - 1]) ^ int.Parse(formulaList[a + 1])).ToString();
@@ -24,6 +78,7 @@ namespace Evaluate
                 }
             }
             */
+
             while (true) //곱하기 나누기 연산
             {
                 int a = 0;
@@ -74,7 +129,7 @@ namespace Evaluate
 
         static double evaluate(string formula)
         {
-            while(true)  //sin cos 연산
+            while (true)  //sin cos 연산
             {
                 if (formula.Contains("sin") || formula.Contains("cos"))
                 {
@@ -87,8 +142,8 @@ namespace Evaluate
                     sinNum = formula.LastIndexOf("sin");
                     cosNum = formula.LastIndexOf("cos");
                     typeNum = (sinNum > cosNum) ? 1 : 0;
-                    
-                    if(typeNum == 1 & sinNum != -1)
+
+                    if (typeNum == 1 & sinNum != -1)
                     {
                         fstFlag = sinNum;
                         secFlag = fstFlag;
@@ -125,39 +180,40 @@ namespace Evaluate
                 int fstFlag = 0;
                 int secFlag = 0;
                 string tmpFormula;
-
-                if (formula.Contains(")"))
+                if (formula.Split('(').Length == formula.Split(')').Length) //괄호의 갯수가 정상인지 판단하고 실행
                 {
-                    if (formula[a] == '(') 
+                    if (formula.Contains(")"))
                     {
-                        fstFlag = a;
-                        while (formula[a] != ')')
+                        if (formula[a] == '(')
                         {
-                            if (formula[a] == '(')
+                            fstFlag = a;
+                            while (formula[a] != ')')
                             {
-                                fstFlag = a;
+                                if (formula[a] == '(')
+                                {
+                                    fstFlag = a;
+                                }
+                                a++;
                             }
-                            a++;
+                            secFlag = a;
+                            tmpFormula = Calculation(CreateList(formula.Substring(fstFlag + 1, secFlag - fstFlag - 1))).ToString(); //괄호 내부 계산
+                            formula = formula.Remove(fstFlag, secFlag - fstFlag + 1); // 사용된 식은 지우고
+                            formula = formula.Insert(fstFlag, tmpFormula); // 계산된 값 저장
+                            a = fstFlag; // a를 마지막 세이브 위치로 복구
                         }
-                        secFlag = a;
-                        tmpFormula = Calculation(formula.Substring(fstFlag + 1, secFlag - fstFlag - 1).Split(' ').ToList()).ToString();
-                        formula = formula.Remove(fstFlag, secFlag - fstFlag + 1);
-                        formula = formula.Insert(fstFlag, tmpFormula);
-                        a = fstFlag;
                     }
                 }
+
             }
 
-
-            string[] formulaArray = formula.Split(' ');
-            List<string> formulaList = formulaArray.ToList();
+            List<string> formulaList = CreateList(formula);
 
             return Calculation(formulaList);
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("계산 식을 입력해주세요");
+            Console.WriteLine("계산식을 입력해주세요");
             Console.WriteLine(evaluate(Console.ReadLine()));
             Console.ReadLine(); // pause
         }
